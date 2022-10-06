6b6cf3b9e34393a6438be0e5b05581b09dfb3583
maria <maria@marias-MacBook-Pro.local>
2022-10-06 07:37:33 +0300

    4th task + comments

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
+from time import time
+# Итерируемся по позициям символов в строке, подсчитывая число одинаковых символов. Все операции производятся
+# со строками, приведенными к нижнему регистру, что бы регистр не влиял на уникальность. Функция вернет первый
+# уникальный символ в нужном регистре, поскольку итерация происходит по позиции в строке.
 def first_non_repeating_letter(string):
     for i in range(len(string)):
         l_string = string.lower()
@@ -6,14 +10,28 @@ def first_non_repeating_letter(string):
     return ''
 
 def make_looper(string):
-    def create_func(string):
-        def create_gen(string):
-            while True:
-                for i in string:
-                    yield (i)
-        gen = create_gen(string)
-        return next(gen)
-    return create_func(string)
+    def create_func():
+        gen = iter(string)
+        return
+    return create_func()
+
+# С помощью рекурсии перебираем все уникальные комбинации номиналов и подсчитываем в переменной n те комбинации,
+# которые удовлетворяют условию. В условии задачи сказано, что функция должна принимать на вход две переменные,
+# в данном варианте решения три переменные, но поскольку при первом вызове функции n всегда = 0, то мы вправе
+# указать n по умолчанию = 0 и передавать только 2 аргумента. Иначе, необходимо создать вложенную функцию и передать
+# в ее аргумент n
+def count_change(sum, denoms, n=0):
+    for i in range(len(denoms)):
+        if sum - denoms[i] > 0:
+            n = count_change(sum - denoms[i], denoms[i:], n)
+        elif sum - denoms[i] == 0:
+            n += 1
+    return n
 
 
-print(a = make_looper('aass'))
+g = make_looper('world')
+print(g())
+print(g())
+print(g())
+print(g())
+print(g())
--- test_tasks.py
+++ test_tasks.py
@@ -1,5 +1,5 @@
 import pytest
-from tasks import first_non_repeating_letter
+from tasks import first_non_repeating_letter, count_change
 
 
 def test_first_non_repeating_letter():
@@ -12,3 +12,7 @@ def test_first_non_repeating_letter():
     assert first_non_repeating_letter('sTreSS') == 'T'
     assert first_non_repeating_letter('Go hang a salami, I\'m a lasagna hog!') == ','
 
+def test_count_change():
+    assert count_change(4, [1, 2]) == 3
+    assert count_change(10, [5, 2, 3]) == 4
+    assert count_change(11, [5, 7]) == 0
