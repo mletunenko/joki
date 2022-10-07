ea8dbbec687ee94cb6592602e1c879bf93604ffb
maria <maria@marias-MacBook-Pro.local>
2022-10-07 09:40:07 +0300

    4th + comments

--- tasks.py
+++ tasks.py
@@ -1,7 +1,10 @@
-from time import time
+from collections import defaultdict
+
+
 # Итерируемся по позициям символов в строке, подсчитывая число одинаковых символов. Все операции производятся
 # со строками, приведенными к нижнему регистру, что бы регистр не влиял на уникальность. Функция вернет первый
-# уникальный символ в нужном регистре, поскольку итерация происходит по позиции в строке.
+# уникальный символ в нужном регистре, поскольку итерация происходит по позиции в строке, а не по символам.
+
 def first_non_repeating_letter(string):
     for i in range(len(string)):
         l_string = string.lower()
@@ -9,24 +12,32 @@ def first_non_repeating_letter(string):
             return string[i]
     return ''
 
-# Потребуется еще одна функция create_gen которая, в которой будет создаваться бесконечный генератор из переданной
+
+# Потребуется еще одна функция create_gen, в которой будет создаваться бесконечный генератор из переданной
 # строки. Функцию wrapper применим, что бы обернуть функцию next(). Поскольку переменная generator для функции wrapper
 # является внешней, состояние генератора будет сохранено.
+
+def create_gen(string):
+    while True:
+        for i in string:
+            yield (i)
+
+
 def make_looper(string):
-    def create_gen(string):
-        while True:
-            for i in string:
-                yield (i)
-    generator =create_gen(string)
+    generator = create_gen(string)
+
     def wrapper():
         return next(generator)
+
     return wrapper
+
 
 # С помощью рекурсии перебираем все уникальные комбинации номиналов и подсчитываем в переменной n те комбинации,
 # которые удовлетворяют условию. В условии задачи сказано, что функция должна принимать на вход две переменные,
 # в данном варианте решения три переменные, но поскольку при первом вызове функции n всегда = 0, то мы вправе
 # указать n по умолчанию = 0 и передавать только 2 аргумента. Иначе, необходимо создать вложенную функцию и передать
-# в ее аргумент n
+# ей n третьим аргументом
+
 def count_change(sum, denoms, n=0):
     for i in range(len(denoms)):
         if sum - denoms[i] > 0:
@@ -36,9 +47,30 @@ def count_change(sum, denoms, n=0):
     return n
 
 
-g = make_looper('world')
-print(g())
-print(g())
-print(g())
-print(g())
-print(g())
+# Выполним две проверки (на "состав" слова и на порядок символов):
+# 1) Проверим что строку s можно составить из строк part1 и part1 сравнив словари, возвращаемые функцией create_counter
+# 2) Проверим что порядок символов в частичке не противоречит порядку в главной строке с помощью функции check_order
+# 3) Все проверки выполним в функции is_merge
+
+def create_counter(s):
+    dict = defaultdict(int)
+    for i in s:
+        dict[i] += 1
+    return dict
+
+
+def check_order(s, part):
+    for i in part:
+        pos = s.find(i)
+        if pos >= 0:
+            s = s[pos:]
+            continue
+        else:
+            return False
+    return True
+
+
+def is_merge(s, part1, part2):
+    if create_counter(s) == create_counter(part1 + part2) and check_order(s, part1) and check_order(s, part2):
+        return True
+    return False
--- test_tasks.py
+++ test_tasks.py
@@ -1,5 +1,4 @@
-import pytest
-from tasks import first_non_repeating_letter, count_change
+from tasks import first_non_repeating_letter, count_change, make_looper, is_merge
 
 
 def test_first_non_repeating_letter():
@@ -12,7 +11,23 @@ def test_first_non_repeating_letter():
     assert first_non_repeating_letter('sTreSS') == 'T'
     assert first_non_repeating_letter('Go hang a salami, I\'m a lasagna hog!') == ','
 
+
 def test_count_change():
     assert count_change(4, [1, 2]) == 3
     assert count_change(10, [5, 2, 3]) == 4
     assert count_change(11, [5, 7]) == 0
+
+
+def test_make_looper():
+    abc = make_looper('abc')
+    assert abc() == 'a'
+    assert abc() == 'b'
+    assert abc() == 'c'
+    assert abc() == 'a'
+
+
+def test_is_merge():
+    assert (is_merge('helloworld', 'hello', 'world')) is True
+    assert (is_merge('helloworld', 'how', 'ellorld')) is True
+    assert (is_merge('helloworld', 'hell', 'world')) is False
+    assert (is_merge('helloworld', 'ehll', 'world')) is False
